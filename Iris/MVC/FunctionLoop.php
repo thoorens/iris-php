fdcabd5e9c91aa42868c1240dd0f2d0bd2ba39e2
Jacques Thoorens <iris@thoorens.net>
2012-09-06 16:50:39 +0200

    Addition of FunctionLoop and the corresponding Floop helper

---
+++ Iris/MVC/FunctionLoop.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Iris\MVC;
+
+use Iris\Engine as ie,
+    Iris\Exceptions as ix;
+
+
+/*
+ * This file is part of IRIS-PHP.
+ *
+ * IRIS-PHP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * IRIS-PHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with IRIS-PHP.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @copyright 2012 Jacques THOORENS
+ */
+
+/**
+ * A view which manages a small part of the page. It can be reused
+ * or used in a loop
+ * 
+ * @author Jacques THOORENS (irisphp@thoorens.net)
+ * @see http://irisphp.org
+ * @license GPL version 3.0 (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id: $ * 
+ */
+class Loop extends \Iris\MVC\Partial {
+
+    /**
+     * Type of view
+     * 
+     * @var string
+     */
+    protected static $_ViewType = 'loop';
+
+
+
+    /*
+     * Returns the name of the directory where to find the script
+     * corresponding to the partial
+     * 
+     * @return string
+     */
+
+    protected function _viewDirectory() {
+        return "scripts/";
+    }
+
+    public function render($dummy=NULL) {
+        ob_start();
+        $prop = $this->_properties;
+        // Normal processing for associative array
+        if(!is_numeric(key($prop))){
+            foreach ($this->_properties as $key => $propertie) {
+                if(!is_array($propertie)){
+                    $propertie = $this->_properties;
+                }
+                $partial = new Partial($this->_viewScriptName, $propertie, $key);
+                echo $partial->render();
+            }
+        }
+        // non associative arrays may be processed too
+        else {
+            unset($this->_properties['CURRENTLOOPKEY']);
+            foreach ($this->_properties as $propertie) {
+                $partial = new Partial($this->_viewScriptName, array(),$propertie);
+                echo $partial->render();
+            }
+        }
+        return ob_get_clean();
+    }
+
+}
+
---
+++ Iris/views/helpers/Floop.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Iris\views\helpers;
+
+/*
+ * This file is part of IRIS-PHP.
+ *
+ * IRIS-PHP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * IRIS-PHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with IRIS-PHP.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @copyright 2012 Jacques THOORENS
+ *
+ * 
+ * @author Jacques THOORENS (irisphp@thoorens.net)
+ * @see http://irisphp.org
+ * @license GPL version 3.0 (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id: $ * 
+ * 
+ */
+
+/**
+ * Loop offers a way to display the items of an array by means of a
+ * partial.
+ * 
+ */
+class Loop extends _ViewHelper {
+
+    
+    public function help($viewName, $dataSet) {
+        $loop = new \Iris\MVC\Loop($viewName,$dataSet);
+        return $loop->render();
+    }
+
+}
+
