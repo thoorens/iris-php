86cd6fe1e2648cffea0b52452a17814a4580506d
Jacques Thoorens <iris@thoorens.net>
2012-12-05 09:27:59 +0100

    The view rendering is now treated by the Template class (some adjustments in workbench)

--- IrisWB/application/config/50_Config.php
+++ IrisWB/application/config/50_Config.php
@@ -2,3 +2,6 @@
 // Test Loader for plain classes:
 //\IrisTest\Engine\Loader::DoTest(1, array('index','transTest'));
 \Iris\Translation\_Translator::SetCurrentTranslator(\Iris\Translation\SystemTranslator::GetInstance());
+
+// In workbench, performances are, usually, not the main point
+\Iris\MVC\Template::setCacheTemplate(\Iris\MVC\Template::CACHE_NEVER);
