bc4455b15ebd15899bbbdb377fc33ff5132fd893
Jacques Thoorens <iris@thoorens.net>
2013-01-18 19:41:08 +0100

    The form module is very unfinished. A first approach.

---
+++ IrisWB/application/modules/forms/controllers/documents.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace modules\forms\controllers;
+
+/**
+ * 
+ * Created for IRIS-PHP 0.9 - beta
+ * Description of documents
+ * 
+ * @author jacques
+ * @license not defined
+ */
+class documents extends _forms {
+
+    public function indexAction() {
+         
+    }
+
+}
