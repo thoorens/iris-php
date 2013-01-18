0084cf8bed5143efc95408e7230c2034a1e01038
Jacques Thoorens <iris@thoorens.net>
2013-01-18 19:33:00 +0100

    Changes in Workbench and Iris/main to show the use of library views (forgotten file)

---
+++ IrisWB/application/modules/main/controllers/system.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace modules\main\controllers;
+
+/**
+ * 
+ * Created for IRIS-PHP 0.9 - beta
+ * Test of the views
+ * 
+ * @author jacques
+ * @license not defined
+ */
+class system extends _main {
+
+    protected function _init() {
+        $this->_setLayout('color');
+        $this->__bodyColor = 'BLUE1';
+    }
+
+    public function indexAction() {
+        $this->__required = '--- implicit ---';
+    }
+
+    public function explicitAction() {
+        $scriptName = 'EXPLICIT';
+        $this->setViewScriptName($scriptName);
+        $this->__required = $scriptName;
+        $this->__reqPartial = 'psystem';
+    }
+
+    public function commonAction() {
+        $scriptPath = 'commons/system';
+        $this->setViewScriptName($scriptPath);
+        $this->__required = $scriptPath;
+        $this->__reqPartial = 'commons/psystem';
+    }
+
+    
+}
