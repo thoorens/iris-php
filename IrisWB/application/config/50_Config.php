f743b83a56a86389e4e0853d5f0364be0ac818f7
Jacques Thoorens <iris@thoorens.net>
2013-02-02 10:58:24 +0100

    Optional parameter for AdminToolBar with Ajax

--- IrisWB/application/config/50_Config.php
+++ IrisWB/application/config/50_Config.php
@@ -5,3 +5,6 @@
 
 // In workbench, performances are, usually, not the main point
 \Iris\MVC\Template::setCacheTemplate(\Iris\MVC\Template::CACHE_NEVER);
+
+// To have an Admintoolbar without Ajax, uncomment the next line
+//\ILO\views\helpers\AdminToolBar::$AjaxMode = \FALSE;
