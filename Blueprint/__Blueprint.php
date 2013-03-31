19485d49a744eeb41defb8efa805ddee5962dfcc
Jacques THOORENS <git@thoorens.net>
2013-03-31 18:41:13 +0300

    Update __Blueprint.php

--- Blueprint/__Blueprint.php
+++ Blueprint/__Blueprint.php
@@ -3,5 +3,7 @@
 /**
  * This is not a library, but some files in "blueprint" state, future or
  * forsaken projects
- * or 
+ * This directory is ignored by GIT and exists only on my private
+ * PC. The new file introduced here are mainly old files from the
+ * main libraries.
  */
