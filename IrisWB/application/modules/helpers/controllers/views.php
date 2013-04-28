8ba95b326eb2d2237d57eea9a144f67f09ed9d2e
Jacques Thoorens <iris@thoorens.net>
2013-04-28 19:15:59 +0200

    WB demo for views helpers

---
+++ IrisWB/application/modules/helpers/controllers/views.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace modules\helpers\controllers;
+
+/**
+ * srv_IrisWB
+ * Created for IRIS-PHP 0.9 - beta
+ * Description of controllers
+ * 
+ * @author jacques
+ * @license not defined
+ */
+class views extends _helpers {
+
+    public function indexAction() {
+        
+    }
+    
+    
+
+}
---
+++ IrisWB/application/modules/helpers/views/helpers/LocalAspect.php
@@ -0,0 +1,49 @@
+<?php
+
+
+
+namespace iris\views\helpers;
+
+/*
+ * This file is part of IRIS-PHP.
+ *
+ * IRIS-PHP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * IRIS-PHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with IRIS-PHP.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @copyright 2012 Jacques THOORENS
+ *
+ * 
+ * @author Jacques THOORENS (irisphp@thoorens.net)
+ * @see http://irisphp.org
+ * @license GPL version 3.0 (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id: $ */
+
+/**
+ * An example of view helper (for Iris WB)
+ * 
+ */
+class LocalAspect extends \Iris\views\helpers\_ViewHelper {
+
+    static $_singleton = FALSE;
+
+    public function help($text) {
+        return <<< HTML
+<span style="margin: 15px; color: #0A0;background: #BFF;border-style: solid;border-color: #00A;">
+    $text
+</span>
+HTML;
+        
+    }
+
+}
+
