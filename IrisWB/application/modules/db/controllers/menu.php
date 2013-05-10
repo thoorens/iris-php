c9d0ec336b906fef4fb52e328d2c2dabcb694b71
Jacques Thoorens <iris@thoorens.net>
2013-05-10 18:07:14 +0200

    DB Work Bench : step 4 left menu comment

--- IrisWB/application/modules/db/controllers/menu.php
+++ IrisWB/application/modules/db/controllers/menu.php
@@ -2,16 +2,40 @@
 
 namespace modules\db\controllers;
 
-/**
- * srv_IrisWB
- * Created for IRIS-PHP 0.9 - beta
- * Description of index
+/*
+ * This file is part of IRIS-PHP.
+ *
+ * IRIS-PHP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * IRIS-PHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with IRIS-PHP.  If not, see <http://www.gnu.org/licenses/>.
  * 
- * @author jacques
- * @license not defined
+ * @copyright 2012 Jacques THOORENS
+ *
+ * 
  */
+
+/**
+ * 
+ * Test of basic crud operations
+ * 
+ * @author Jacques THOORENS (irisphp@thoorens.net)
+ * @see http://irisphp.org
+ * @license GPL version 3.0 (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id: $ */
 class menu extends \Iris\MVC\_Subcontroller {
 
+    /**
+     * A menu for the left part of the screen
+     */
     public function indexAction() {
         
     }
