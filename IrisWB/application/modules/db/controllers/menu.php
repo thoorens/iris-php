db9e6319f93c4f0206fdbde80f54784edc8077d0
Jacques Thoorens <iris@thoorens.net>
2013-05-10 17:57:43 +0200

    DB Work Bench : step 3 left menu

---
+++ IrisWB/application/modules/db/controllers/menu.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace modules\db\controllers;
+
+/**
+ * srv_IrisWB
+ * Created for IRIS-PHP 0.9 - beta
+ * Description of index
+ * 
+ * @author jacques
+ * @license not defined
+ */
+class menu extends \Iris\MVC\_Subcontroller {
+
+    public function indexAction() {
+        
+    }
+    
+    
+
+}
