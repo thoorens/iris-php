1429b661461b26d5338053f7bddb9010563f4cb0
Jacques Thoorens <iris@thoorens.net>
2013-09-09 11:19:41 +0200

    New implementation of Settings (stage 1)

--- CLI/Files/application/20_settings.php
+++ CLI/Files/application/20_settings.php
@@ -1,7 +1,7 @@
 <?php
 // If Dojo is used, you must provide a source. In development it is used in error display.
-// The first 3 sources need an access to Internet to function
-\Dojo\Manager::SetSource(\Dojo\Manager::GOOGLE);
-//\Dojo\Manager::SetSource(\Dojo\Manager::YANDEX);
+// The first 2 sources need an access to Internet to function
+//\Dojo\Engine\Settings::SetSource(\Dojo\Manager::GOOGLE);
+//\Dojo\Engine\Settings::SetSource(\Dojo\Manager::YANDEX);
 // if LOCAL, you must copy or link dojo, dojox and dijit folders in /public/js
-//\Dojo\Manager::SetSource(\Dojo\Manager::LOCAL);
+//\Dojo\Engine\Settings::SetSource(\Dojo\Manager::LOCAL);
