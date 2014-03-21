27d411dad59be85d1048782d85ec1ce7efea0c75
jacques <jacques@localhost>
2014-03-21 07:08:52 +0100

    CrudIconManager adaptation + documentation updated

--- Iris/views/helpers/Browser.php
+++ Iris/views/helpers/Browser.php
@@ -18,7 +18,7 @@ namespace Iris\views\helpers;
  * You should have received a copy of the GNU General Public License
  * along with IRIS-PHP.  If not, see <http://www.gnu.org/licenses/>.
  * 
- * @copyright 2012 Jacques THOORENS
+ * @copyright 2011-22014 Jacques THOORENS
  */
 
 /**
@@ -32,13 +32,24 @@ namespace Iris\views\helpers;
  */
 class Browser extends _ViewHelper {
 
+    /**
+     * The update button is to be added or not
+     */
     const UPDATE = 1;
+    /**
+     * The delete button is to be added or not
+     */
     const DELETE = 2;
+    /**
+     * The create button is to be added or not
+     */
     const CREATE = 4;
+    /**
+     * The find button is to be added or not
+     */
     const FIND = 8;
 
     protected static $_Singleton = TRUE;
-
 
     /**
      * Displays first, previous, next, last buttons + one to 4 buttons for 
@@ -48,23 +59,26 @@ class Browser extends _ViewHelper {
      * @param boolean $active: if false, the browser is not active
      * @return string html code
      */
-    public function help($edit=15, $active=TRUE) {
-        $crudHelper = \Iris\Subhelpers\Crud::GetInstance(NULL);
+    public function help($edit = 15, $active = TRUE) {
+        $crudHelper = \Iris\Subhelpers\CrudIconManager::GetInstance(NULL);
         $html = '<span class="Iris_Browser">';
         $html .= $crudHelper->render('first', $active);
         $html .= $crudHelper->render('previous', $active);
-        if ($edit & self::UPDATE)
+        if ($edit & self::UPDATE) {
             $html .= $crudHelper->render('update', $active);
-        if ($edit & self::DELETE)
+        }
+        if ($edit & self::DELETE) {
             $html .= $crudHelper->render('delete', $active);
-        if($edit & self::FIND)
+        }
+        if ($edit & self::FIND) {
             $html .= $crudHelper->render('find', $active);
+        }
         $html .= $crudHelper->render('next', $active);
         $html .= $crudHelper->render('last', $active);
-        if ($edit & self::CREATE)
+        if ($edit & self::CREATE) {
             $html .= $crudHelper->render('create', $active);
+        }
         return "$html</span>\n";
     }
 
 }
-
