2a2ae6fc5ae95d7af017fc18f94138ce89753cd5
jacques <jacques@localhost>
2014-05-17 09:13:34 +0200

    PHP Markdown taken from Michel Fortin (see license information in various files)

---
+++ Vendors/Markdown/__Markdown.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This library is a creation of Michel Fortin (based on the work of John Gruber .
+ * See the file License.md in the directory for a complete description 
+ * of the work.
+ * 
+ * The file are taken from an archive downloaded on April 26 2014
+ * https://github.com/michelf/php-markdown
+ * 
+ * For other explanations, see Michel Fortin's site:
+ * http://michelf.ca/projets/php-markdown/
+ * 
+ * Map of the original directory
+ * 
+ * php-markdown-lib/                  (renamed Markdown)
+ * ├── composer.json                  (left unchanged)
+ * ├── License.md                     (left unchanged)
+ * ├── Michelf                        (deleted and three files moved to main directory)
+ * │   ├── MarkdownExtra.inc.php      (deleted: autoload is functional in IRIS-PHP)
+ * │   ├── MarkdownExtra.php          (moved to main directory, namespace modified)                                                                                                                                  
+ * │   ├── Markdown.inc.php           (deleted: autoload is functional in IRIS-PHP)                                                                                                                                         
+ * │   ├── MarkdownInterface.inc.php  (deleted: autoload is functional in IRIS-PHP)                                                                                                                                  
+ * │   ├── MarkdownInterface.php      (moved to main directory, namespace modified)                                                                                                                                  
+ * │   └── Markdown.php               (moved to main directory, namespace modified)                                                                                                                                  
+ * ├── Readme.md                      (left unchanged)                                                                                                                                                     
+ * └── Readme.php                     (left unchanged)
+ */
+
+/**
+ * 
+ */
+
+/*
+ * The orginal namespace defined by Michel Fortin has been renamed
+ */
+namespace Markdown;
---
+++ Vendors/__Vendors.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of IRIS-PHP.
+ *
+ * IRIS-PHP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * IRIS-PHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with IRIS-PHP.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @copyright 2011-2014 Jacques THOORENS
+ *
+ * 
+ */
+
+/**
+ * The Vendors library is composed of distinct external libraries written 
+ * by other programmers.
+ * 
+ * Each of them are copyrighted with the licence defined by its authro.
+ * Please read the copyright material in each of the subdirectories.
+ */
+namespace Vendors;
