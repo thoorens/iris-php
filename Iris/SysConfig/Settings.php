924ca69609a1238072da6f847ba5b0a6926bf5d5
jacques <jacques@localhost>
2015-03-24 11:55:40 +0100

    New copyright header

--- Iris/SysConfig/Settings.php
+++ Iris/SysConfig/Settings.php
@@ -3,22 +3,12 @@
 namespace Iris\SysConfig;
 
 /*
- * This file is part of IRIS-PHP.
- *
- * IRIS-PHP is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * IRIS-PHP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with IRIS-PHP.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @copyright 2012 Jacques THOORENS
+ * This file is part of IRIS-PHP, distributed under the General Public License version 3.
+ * A copy of the GNU General Public Version 3 is readable in /library/gpl-3.0.txt.
+ * More details about the copyright may be found at
+ * <http://irisphp.org/copyright> or <http://www.gnu.org/licenses/>
+ *  
+ * @copyright 2011-2015 Jacques THOORENS
  */
 
 /**
@@ -68,6 +58,8 @@ class Settings extends _Settings {
         StandardSetting::CreateSetting('defaultRoleName', 'browse');
         StandardSetting::CreateSetting('defaultUserEmail', 'info@irisphp.org');
         StandardSetting::CreateSetting('systemUserEntity', '');
+        // Type of Password
+        StandardSetting::CreateSetting('defaultHashType', \Iris\Users\_Password::MODE_PHP55);
         //
         StandardSetting::CreateSetting('errorDebuggingLevel', 1);
         // To minimize execution templates can be cached (not by default)
@@ -93,8 +85,7 @@ class Settings extends _Settings {
         StandardSetting::CreateSetting('dataFolder', IRIS_ROOT_PATH . '/data');
         // The default translator is
         StandardSetting::CreateSetting('defaultTranslator', '\Iris\Translation\NullTranslator');
-        // Type of Password
-        StandardSetting::CreateSetting('passwordHashType', PASSWORD_BCRYPT);
+        
     }
 
 }
