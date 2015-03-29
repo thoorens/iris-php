acb69a3e4fa4d0dca42223d32013b5de461d7d46
jacques <jacques@localhost>
2015-03-29 13:37:17 +0200

    New copyright header

--- Dojo/views/helpers/_DojoHelper.php
+++ Dojo/views/helpers/_DojoHelper.php
@@ -2,28 +2,17 @@
 
 namespace Dojo\views\helpers;
 
-/* 
- * This file is part of IRIS-PHP.
- *
- * IRIS-PHP is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * IRIS-PHP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with IRIS-PHP.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * @copyright 2012 Jacques THOORENS
- *
+/*
+ * This file is part of IRIS-PHP, distributed under the General Public License version 3.
+ * A copy of the GNU General Public Version 3 is readable in /library/gpl-3.0.txt.
+ * More details about the copyright may be found at
+ * <http://irisphp.org/copyright> or <http://www.gnu.org/licenses/>
+ *  
+ * @copyright 2011-2015 Jacques THOORENS
  */
 
 /**
- * An superclass mother of all Dojo helpers
+ * A superclass mother of all Dojo helpers
  * 
 * 
  * @author Jacques THOORENS (irisphp@thoorens.net)
@@ -42,6 +31,26 @@ abstract class _DojoHelper extends \Iris\views\helpers\_ViewHelper{
         $this->_manager = \Dojo\Manager::GetInstance();
     }
 
+    /**
+     * This version of getTranslator overrides the trait and tries to
+     * get the translations from  \Dojo\Translation\SystemTranslator
+     * 
+     * @staticvar null $translator
+     * @param boolean $system by default the system translation is taken
+     * @return \Dojo\Translation\SystemTranslator
+     */
+    public function getTranslator($system = \TRUE) {
+        static $translator = NULL;
+        if (is_null($translator)) {
+            if ($system) {
+                $translator = \Dojo\Translation\SystemTranslator::GetInstance();
+            }
+            else {
+                $translator = \Iris\Translation\_Translator::GetInstance();
+            }
+        }
+        return $translator;
+    }
 }
 
 
