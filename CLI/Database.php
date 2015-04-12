041dff3fae3b6b8d3afbb3eaa2e78b3a57f10819
jacques <jacques@localhost>
2015-04-12 19:07:13 +0200

    New header with modified copyright informations in CLI

--- CLI/Database.php
+++ CLI/Database.php
@@ -2,6 +2,17 @@
 
 namespace CLI;
 
+
+/*
+ * This file is part of IRIS-PHP, distributed under the General Public License version 3.
+ * A copy of the GNU General Public Version 3 is readable in /library/gpl-3.0.txt.
+ * More details about the copyright may be found at
+ * <http://irisphp.org/copyright> or <http://www.gnu.org/licenses/>
+ *  
+ * @copyright 2011-2015 Jacques THOORENS
+ */
+
+
 define('IRIS_DB_FOLDER', '/config/base/');
 define('IRIS_DB_DEMOFILE', 'demo.sqlite');
 define('IRIS_DB_INIFILE', '10_database.ini');
