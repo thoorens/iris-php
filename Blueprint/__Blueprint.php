29dc5db9265b1eb23a13fb96c3e276a30ae260b1
jacques <jacques@naxos>
2015-07-27 10:59:02 +0200

    A typo

--- Blueprint/__Blueprint.php
+++ Blueprint/__Blueprint.php
@@ -1,10 +1,11 @@
 <?php
+/* THIS FILE IS OBSOLETE */
 
 /**
  * This is not a library, but some files in "blueprint" state, future or
  * forsaken projects
  * This directory is ignored by GIT and exists only on my private
- * PC. The new file introduced here are mainly old files from the
+ * PC. The new files introduced here are mainly old files from the
  * main libraries.
  * 
  */
