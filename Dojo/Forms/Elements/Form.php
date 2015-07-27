521d5b9dc2dc2e4cc97d023c38438caa203bd8df
jacques <jacques@naxos>
2015-07-27 10:49:51 +0200

    New copyright header

--- Dojo/Forms/Elements/Form.php
+++ Dojo/Forms/Elements/Form.php
@@ -1,39 +1,26 @@
 <?php
 
-
-
 namespace Dojo\Forms\Elements;
 
 /*
- * This file is part of IRIS-PHP.
- *
- * IRIS-PHP is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * IRIS-PHP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with IRIS-PHP.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * @copyright 2012 Jacques THOORENS
+ * This file is part of IRIS-PHP, distributed under the General Public License version 3.
+ * A copy of the GNU General Public Version 3 is readable in /library/gpl-3.0.txt.
+ * More details about the copyright may be found at
+ * <http://irisphp.org/copyright> or <http://www.gnu.org/licenses/>
+ *  
+ * @copyright 2011-2015 Jacques THOORENS
  */
 
- /**
+/**
  * The Dojo version of the Form class
  * 
  * @author Jacques THOORENS (irisphp@thoorens.net)
  * @see http://irisphp.org
  * @license GPL version 3.0 (http://www.gnu.org/licenses/gpl.html)
  * @version $Id: $ */
-
 class Form extends \Iris\Forms\Elements\Form {
 
-    public function __construct($name, $action=NULL, $method='post') {
+    public function __construct($name, $action = \NULL, $method = 'post') {
         parent::__construct($name, $action, $method);
         \Dojo\Engine\Bubble::GetBubble('dojoForm')
                 ->addModule("dojo/parser")
@@ -45,5 +32,3 @@ class Form extends \Iris\Forms\Elements\Form {
     }
 
 }
-
-
