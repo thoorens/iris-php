acdbc1c14e85ea7f70f71fc9b7f22367a7ca06ba
jacques <jacques@naxos>
2015-09-17 23:43:44 +0200

    Renaming \Dojo\Subhelpers\Link to \Dojo\Subhelpers\Button

---
+++ Dojo/Subhelpers/Button.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Dojo\Subhelpers;
+
+/*
+ * This file is part of IRIS-PHP, distributed under the General Public License version 3.
+ * A copy of the GNU General Public Version 3 is readable in /library/gpl-3.0.txt.
+ * More details about the copyright may be found at
+ * <http://irisphp.org/copyright> or <http://www.gnu.org/licenses/>
+ *  
+ * @copyright 2011-2015 Jacques THOORENS
+ */
+
+/**
+ * This class is a subhelper for the dojo helper Link family. 
+ * 
+ * @author Jacques THOORENS (irisphp@thoorens.net)
+ * @see http://irisphp.org
+ * @license GPL version 3.0 (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id: $ */
+class Button extends \Iris\Subhelpers\Button {
+
+    protected static $_Instance = NULL;
+
+    protected function _javascriptButton() {
+        $this->addAttribute('data-dojo-type', 'dijit/form/Button');
+        $html = parent::_javascriptButton();
+        //$this->removeAttribute('data-dojo-type');
+        return $html;
+    }
+
+    protected function _render() {
+        if ($this->_empty($this->getLabel())) {
+            $text = '';
+        }
+        elseif (!\Iris\Users\Session::JavascriptEnabled() or self::$NoJavaForce) {
+            if (\Iris\System\Client::OldBrowser(self::$OldBrowser)) {
+                $text = $this->_simulatedButton();
+            }
+            else {
+                $text = $this->_linkButton();
+            }
+        }
+        else {
+            $text = $this->_javascriptButton();
+        }
+        return $text;
+    }
+
+}
