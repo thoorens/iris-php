e55c9ea7ecc843f2d06e7f47c60bd56dac0ad3e7
jacques <jacques@naxos>
2015-10-02 14:34:11 +0200

    New copyright header

--- Iris/views/helpers/tViewHelperCaller.php
+++ Iris/views/helpers/tViewHelperCaller.php
@@ -3,29 +3,12 @@
 namespace Iris\views\helpers;
 
 /*
- * This file is part of IRIS-PHP.
- *
- * IRIS-PHP is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * IRIS-PHP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with IRIS-PHP.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * @copyright 2012 Jacques THOORENS
- *
- * 
- * @author Jacques THOORENS (irisphp@thoorens.net)
- * @see http://irisphp.org
- * @license GPL version 3.0 (http://www.gnu.org/licenses/gpl.html)
- * @version $Id: $ * 
- * 
+ * This file is part of IRIS-PHP, distributed under the General Public License version 3.
+ * A copy of the GNU General Public Version 3 is readable in /library/gpl-3.0.txt.
+ * More details about the copyright may be found at
+ * <http://irisphp.org/copyright> or <http://www.gnu.org/licenses/>
+ *  
+ * @copyright 2011-2015 Jacques THOORENS
  */
 
 /**
@@ -43,7 +26,7 @@ trait tViewHelperCaller {
     public function callViewHelper($helperName) {
         // gets optional parameters
         $args = func_get_args();
-        array_shift($args); // helperName is 
+        array_shift($args); // helperName is the only mandatory argument
         $view = isset($this->_view) ? $this->_view : \NULL;
         return \Iris\views\helpers\_ViewHelper::HelperCall($helperName, $args, $view);
     }
