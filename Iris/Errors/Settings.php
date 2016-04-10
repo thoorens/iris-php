3334b85e42dcc55b6b244fc80ef790d9c4e088e0
jacques <jacques@naxos2>
2016-04-10 16:04:35 +0200

    $LogFile inited by __ClassInit()

--- Iris/Errors/Settings.php
+++ Iris/Errors/Settings.php
@@ -1,33 +1,13 @@
 <?php
-
 namespace Iris\Errors;
 
 /*
- * This file is part of IRIS-PHP.
- *
- * IRIS-PHP is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * IRIS-PHP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with IRIS-PHP.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * @copyright 2012 Jacques THOORENS
- *
- * 
- * @author Jacques THOORENS (irisphp@thoorens.net)
- * @see http://irisphp.org
- * @license GPL version 3.0 (http://www.gnu.org/licenses/gpl.html)
- * @version $Id: $ * 
- * Thank you to Eric Daspet and Cyril Pierre de Geyser
- * for their clear explanations in their book
- * "PHP avancé" (Editions Eyrolles)
+ * This file is part of IRIS-PHP, distributed under the General Public License version 3.
+ * A copy of the GNU General Public Version 3 is readable in /library/gpl-3.0.txt.
+ * More details about the copyright may be found at
+ * <http://irisphp.org/copyright> or <http://www.gnu.org/licenses/>
+ *  
+ * @copyright 2011-2016 Jacques THOORENS
  */
 
 /**
@@ -90,7 +70,7 @@ class Settings  {
      * Sets a dault name for the logfile
      * @var string
      */
-    public static $LogFile =  '/log/iris.log';
+    public static $LogFile =  \NULL;
     
     /**
      * By default forbits the handler from sending an email to the administrator
@@ -147,6 +127,10 @@ class Settings  {
 //        $this->_forceSettings();
 //    }
 
+    public static function __ClassInit() {
+        self::$LogFile = IRIS_ROOT_PATH . '/' . $application . '/log/error.log';
+    }
+    
     /**
      * Gets from URL optional ERROR parameters and
      * modifies the settings accordingly
