0837ad7f2d82928840cddaab3d01d8d071e477b9
jacques <jacques@naxos2>
2016-10-03 17:43:33 +0200

    Model definitions for autoform tests (beta state)

--- IrisWB/application/models_internal/TAutoform.php
+++ IrisWB/application/models_internal/TAutoform.php
@@ -2,6 +2,7 @@
 
 namespace models_internal;
 
+
 /*
  * This file is part of IRIS-PHP.
  *
@@ -22,27 +23,21 @@ namespace models_internal;
  */
 
 /**
- * Internal DB of IrisWB (see config/base/config.sqlite)
- * 
- * A wrapper class for \Iris\Structure\_TSequence: it permits instanciation
- * and naming and connexion to database definition. The sequence table contains
- * all the screens of the show.
- * 
- * This class is used as a 
- * 
- * Project IRIS-PHP
+ * An old version misplaced of the test for magickforms 
  * 
  * @author Jacques THOORENS (irisphp@thoorens.net)
  * @see http://irisphp.thoorens.net
  * @license GPL version 3.0 (http://www.gnu.org/licenses/gpl.html)
- * @version $Id: $ */
+ * @version $Id: $ 
+ * @deprecated placed in normal models
+ */
 class TAutoform extends \Iris\DB\_Entity {
 
     protected $_idNames = ['id'];
 
     public static function Create() {
         $sql = 'CREATE TABLE autoform (id INTEGER PRIMARY KEY  NOT NULL, Name TEXT, Hidden TEXT, EventDate DATETIME, EventHour DATETIME, Password TEXT, Checkbox BOOL)';
-        $em = \Iris\DB\_EntityManager::EMByNumber(); //GetInstance();
+        $em = \Iris\DB\_EntityManager::EMByNumber(1); //GetInstance();
         $list = $em->listTables();
         if (!in_array('autoform', $list)) {
             $em->directSQLExec($sql);
