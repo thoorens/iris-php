cfd7c69a74dca5c0096516446bad964308316528
jacques <jacques@naxos2>
2017-04-15 01:14:59 +0200

    All files in BluePrint now have an equivalent to keep access to git archive files (step 4)

--- Iris/views/helpers/IrisCSS.php
+++ Iris/views/helpers/IrisCSS.php
@@ -1,59 +1,25 @@
 <?php
 
-namespace Iris\views\helpers;
-
 /*
- * This file is part of IRIS-PHP.
- *
- * IRIS-PHP is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * IRIS-PHP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with IRIS-PHP.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * @copyright 2012 Jacques THOORENS
- *
- * 
- * @author Jacques THOORENS (irisphp@thoorens.net)
- * @see http://irisphp.org
- * @license GPL version 3.0 (http://www.gnu.org/licenses/gpl.html)
- * @version $Id: $ * 
+ * This file is part of IRIS-PHP, distributed under the General Public License version 3.
+ * A copy of the GNU General Public Version 3 is readable in /library/gpl-3.0.txt.
+ * More details about the copyright may be found at
+ * <http://irisphp.org/copyright> or <http://www.gnu.org/licenses/>
+ *  
+ * @copyright 2011-2017 Jacques THOORENS
  */
 
 /**
- * This helper loads css styles used by Log and 
- * internal (intended to be put in <head> )
+ * This help method permited to charge irisColor.css and iris.css
  * 
+ * This file has been moved to  BluePrint archives
+ * 
+ * Project IRIS-PHP
+ *
+ * @author Jacques THOORENS (irisphp@thoorens.net)
+ * @see http://irisphp.org
+ * @license GPL version 3.0 (http://www.gnu.org/licenses/gpl.html)
+ * @deprecated since version 2017/02
+ * @version :$Id:
  */
-class IrisCSS extends _ViewHelper {
-    
-    
-
-    
-    public function help($force=FALSE) {
-        $log = \Iris\Engine\Log::GetInstance();
-        // in development mode, a simplified CSS to manage PAGE logs
-        if (\Iris\Engine\Mode::IsDevelopment() and $log->getPosition()==\Iris\Engine\Log::POS_PAGE){
-            return <<<HTML
-<link href="/iris_aspect/css/iris_debug.css" rel="stylesheet" type="text/css" />
-HTML;
-        }
-        // some layouts may force a complete loading of iris styles
-        elseif($force){
-            return <<<HTML
-<link href="/iris_aspect/css/iris.css" rel="stylesheet" type="text/css" />
-<link href="/iris_aspect/css/irisColor.css" rel="stylesheet" type="text/css" />
-HTML;
-        }
-    }
-
-}
-
 
