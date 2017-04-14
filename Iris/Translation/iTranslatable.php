aa3a4e2c261661fe1445c8e76da8ef54f15cdd1f
jacques <jacques@naxos2>
2017-04-15 01:16:48 +0200

    All files in BluePrint now have an equivalent to keep access to git archive files (step 4)

--- Iris/Translation/iTranslatable.php
+++ Iris/Translation/iTranslatable.php
@@ -1,50 +1,26 @@
 <?php
 
-
-
-namespace Iris\Translation;
-
 /*
- * This file is part of IRIS-PHP.
- *
- * IRIS-PHP is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * IRIS-PHP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with IRIS-PHP.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * @copyright 2012 Jacques THOORENS
+ * This file is part of IRIS-PHP, distributed under the General Public License version 3.
+ * A copy of the GNU General Public Version 3 is readable in /library/gpl-3.0.txt.
+ * More details about the copyright may be found at
+ * <http://irisphp.org/copyright> or <http://www.gnu.org/licenses/>
+ *  
+ * @copyright 2011-2017 Jacques THOORENS
+ */
+
+/**
+ * All classes implementing the old interface iTranslatable use now the tTranslatable or tSystemTranslatable traits
  *
  * 
+ * This file has been moved to  BluePrint archives
+ * 
+ * Project IRIS-PHP
+ *
  * @author Jacques THOORENS (irisphp@thoorens.net)
  * @see http://irisphp.org
  * @license GPL version 3.0 (http://www.gnu.org/licenses/gpl.html)
- * @version $Id: $ */
-
-/**
- * Interface for provide translation in any class. It has been substituated by the
- * use of various traits which implements its intent.
- * 
+ * @deprecated since version 
+ * @version :$Id:
  */
-interface iTranslatable {
-    
-    /**
-     * @return _Translator
-     */
-  public function getTranslator();
-    
-    /**
-     * @return string
-     */
-  public function _($message,$system=\FALSE);
-    
-}
-
 
